a87eb2af3665d1b505358f1c2079db5324dcc592
Peter Particle <ParticlePeter@gmx.de>
2017-01-07 22:30:49 +0100

    Trackball: fovy and window height are required to correctly compute panning (xform) such that objects in focus stay under the mouse pointer. fixed

--- source/dlsl/trackball.d
+++ source/dlsl/trackball.d
@@ -39,7 +39,7 @@ private:
 
 	vec3 	m_target 		= vec3( 0 );
 	float	m_recipFocus	= 1.0f;
-	float	m_recipHeight	= 0.01f;
+	float	m_recipHeight	= 0.002f;
 	bool	m_dirty			= false;	
 
 	void abs2rel( ref float x, ref float y ) {
@@ -66,25 +66,24 @@ public:
 		lookAt( eye, target, up );
 	}
 
-	void xformVelocity( float vel )		{ m_velXform  = vel; }
-	void orbitVelocity( float vel )		{ m_velOrbit = vel; }
-	void dollyVelocity( float vel )		{ m_velDolly = vel; }
-	mat4 matrix() 						{ m_dirty = false; return m_matrix; }
-	void focus(  float f )				{ m_recipFocus  = 1 / f; }
-	void height( float h )				{ m_recipHeight = 2 / h; }
-	bool dirty()  { return m_dirty; }
-	//void dirty( bool b ) { m_dirty = b; }
+	void xformVelocity( float vel )			{ m_velXform  = vel; }
+	void orbitVelocity( float vel )			{ m_velOrbit = vel; }
+	void dollyVelocity( float vel )			{ m_velDolly = vel; }
 
-	void height_and_focus_from_fovy( float height, float fovy ) {
-		import std.math : tan;
-		m_recipHeight	= 2 / height;
-		m_recipFocus	= - 4 * tan( 0.5f * fovy ) / height;
-	}
+	// TODO(pp): explain why the 3 functions bellow are required to properly compute panning (xform)
+	void height( float h )					{ m_recipHeight = 2 / h; }
+	void fovy( float f )					{ import std.math : tan; m_recipFocus = tan( deg2rad * 0.5f * f ); }
+	void fovy_height( float f, float h )	{ height( h ); fovy( f ); }
 
+	mat4 matrix()							{ m_dirty = false; return m_matrix; }
+	bool dirty()							{ return m_dirty; }
+
+	
+	// set 2D reference point for each navigation gesture (e.g. any click)
 	void reference( float x, float y )	{
 		m_absX = x;
 		m_absY = y;
-	}		// set 2D reference point for each navigation gesture (e.g. any click)
+	}
 
 	mat4 viewMatrix() {
 		m_dirty = false; return m_matrix; 
@@ -98,11 +97,9 @@ public:
 		update;
 	}
 
-
+	// use x and y vectors from rotation matrix to compute camera movement parallel to screen
 	void xform( float x, float y ) {
 		abs2rel( x, y );
-		//float d = abs( camDolly );	/// Tweak the distance to Rotation center, this Z Value gets rotated by Rotation Matrix
-		/// Use X and Y Vectors from Rotation Matrix to calculate Camera Offset Parallel to Screen. Both use camRes.x, otherwise different speed in x and y !
 		m_target += m_velXform * m_recipFocus * m_recipHeight * m_dolly * ( x * vec3( m_matrix[0].x, m_matrix[1].x, m_matrix[2].x ) - y * vec3( m_matrix[0].y, m_matrix[1].y, m_matrix[2].y )); // Both use camRes.x, otherwise different speed in x and y
 		update;
 	}
@@ -110,24 +107,24 @@ public:
 
 	void dolly( float x, float y ) {
 		abs2rel( x, y );
-		float d  = m_dolly;
-		m_dolly -= m_velDolly * m_recipHeight * m_recipFocus * m_dolly * ( x + y );
+		float d = m_dolly;
+		m_dolly -= m_velDolly * m_recipHeight * m_recipFocus * d * ( x + 4 * y );
 		if ( m_dolly < 0.001f ) m_dolly = 0.001f;
 		update;
-		//m_matrix[ 3 ].xyz = m_matrix[ 3 ].xyz * ( m_dolly - dolly );
 	}
 
 	void dolly( float d ) {
 		m_dolly = d < 0.001f ? 0.001f : d;
+		update;
 		m_dirty = true;
 	}
 
-	float  dolly() const {
+	float dolly() const {
 		return m_dolly;
 	}
 
 
-	/// look at function with two points and an up vector
+	/// look at function with two points and an up vector, sets inner state of Trackball
 	void lookAt( vec3 eye, vec3 target = vec3( 0 ), vec3 up = vec3( 0, 1, 0 )) {
 		vec3 vecZ	= eye - target;	// vector from target to eye equals the camera z axis as camera look direction is neagtive z
 		m_dolly		= length( eye - target );
@@ -150,7 +147,7 @@ public:
 }
 
 
-/// look at function with two points and an up vector
+/// look at function with two points and an up vector, returns a matrix
 auto lookAt( vec3 eye, vec3 target = vec3( 0 ), vec3 up = vec3( 0, 1, 0 )) {
 	vec3 vecZ = normalize( eye - target );	// vector from target to eye equals the camera z axis as camera look direction is negative z
 	vec3 vecX = normalize( cross( up, vecZ ));
